863e839a25c0936482e7dd4677272066dbae986d
Martin Voldán <voldanvoldy@gmail.com>
2018-04-26 14:07:26 +0200

    init commit

---
+++ asteroidy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 13:26:11 2018
+
+@author: vol35098
+"""
+
+import pyglet
+
+window = pyglet.window.Window()
+pyglet.app.run()
+print('Hotovo!')
