a404f72bf2d7f566241dbffd559e07e4d00abe4c
Martin Voldán <voldanvoldy@gmail.com>
2018-05-10 14:07:11 +0200

    slppůa

--- asteroidy.py
+++ asteroidy.py
@@ -5,21 +5,49 @@ Created on Thu Apr 26 13:26:11 2018
 
 @author: vol35098
 """
-
+from math import sin, cos, pi
 import pyglet
-from pyglet.window.key import MOD_CTRL, A, B, C
+from pyglet.window.key import DOWN, UP, LEFT, RIGHT
+from pyglet.window.mouse import LEFT as mLEFT
+from pyglet.window.mouse import LEFT as mRIGHT
 
 window = pyglet.window.Window()
 
 obrazek = pyglet.image.load('obrazek.png')
-obrazek.anchor_x = obrazek.width // 2       #nefunguje
-obrazek.anchor_y = obrazek.height // 2       #nefunguje
+obrazek.anchor_x = obrazek.width // 2       
+obrazek.anchor_y = obrazek.height // 2       
 sprite = pyglet.sprite.Sprite(obrazek)
 
 
-def tiktak(t):
-    sprite.x = sprite.x + 10*t 
+uhel = sprite.rotation = 60
+rychlost = 10
+klavesy = set ()
 
+def tiktak(t):
+    global rychlost
+    global uhel
+    #posun
+    sprite.x = sprite.x + rychlost*t*sin(pi*uhel/180)
+    sprite.y = sprite.y + rychlost*t*cos(pi*uhel/180)
+   
+    #natoceni   
+    for sym in klavesy:
+        if sym == DOWN:
+            if rychlost < 10:
+                rychlost = 0
+            else:
+                rychlost -= 10
+        if sym == UP:
+            if rychlost > 90:
+                rychlost = 100
+            rychlost += 10
+        if sym == LEFT:
+            uhel -= 10
+            sprite.rotation -= 10
+        if sym == RIGHT:
+            uhel += 10
+            sprite.rotation += 10
+            sprite.rotation += 10
 
 @window.event
 def on_draw():
@@ -28,22 +56,32 @@ def on_draw():
 
 @window.event
 def on_mouse_press(x, y, button, mod):
-    if button == 1:
+    global uhel
+    if button == mLEFT:
         sprite.x = x
         sprite.y = y
-    elif button == 4:
+    elif button == mRIGHT and (mod & MOD_SHIFT):
+        sprite.rotation += 180
+        uhel += 180
+    elif button == mRIGHT and (mod & MOD_CTRL):
+        sprite.rotation += 90
+        uhel +=90
+    elif button == mRIGHT:
         sprite.rotation += 10
+        uhel += 10
 
 @window.event
 def on_key_press(sym, mod):
-    if sym !=A:
-        print(sym, mod)
+    global klavesy 
+    klavesy.add(sym)
+    
+@window.event
+def on_key_release(sym, mod):
+    global klavesy 
+    klavesy.remove(sym) 
     
 
-
-
-
-pyglet.clock.schedule_interval(tiktak, 1/10)
+pyglet.clock.schedule_interval(tiktak, 1/25)
 
 pyglet.app.run()        #vždycky na konci
 print('Hotovo!')
